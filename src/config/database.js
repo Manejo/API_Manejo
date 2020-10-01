bd64c095f42c07aaba2e02bf147935d02895c9f5
Barrosbg <61169154+Barrosbg@users.noreply.github.com>
2020-09-30 21:50:10 -0300

    Initial commit
    Migrtions e Seeders

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  username: "postgres",
+  password: "123",
+  database: "database_manejo",
+  host: "127.0.0.1",
+  dialect: "postgres",
+  operetorsAliases: false,
+  define:{
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
